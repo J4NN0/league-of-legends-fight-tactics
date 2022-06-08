73a86ba2754c67a3a14a4d7d148e318c7e02731e
J4NN0 <giannofederico@gmail.com>
2022-06-08 23:15:14 +0200

    move mock on tests

--- pkg/httpclient/request_test.go
+++ pkg/httpclient/request_test.go
@@ -1,34 +1,11 @@
 package httpclient
 
 import (
-	"bytes"
-	"encoding/json"
 	"github.com/stretchr/testify/assert"
-	"io/ioutil"
+	"league-of-legends-fight-tactics/tests/mock"
 	"net/http"
 	"testing"
 )
-
-func mockResponse(obj interface{}, status int) *http.Response {
-	jsonMarshal, _ := json.Marshal(obj)
-	return &http.Response{
-		StatusCode: status,
-		Body:       ioutil.NopCloser(bytes.NewReader(jsonMarshal)),
-		Header:     make(http.Header),
-	}
-}
-
-type RoundTripFunc func(req *http.Request) *http.Response
-
-func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
-	return f(req), nil
-}
-
-func newTestClient(fn RoundTripFunc) *http.Client {
-	return &http.Client{
-		Transport: RoundTripFunc(fn),
-	}
-}
 
 type MockStruct struct {
 	Field1 string
@@ -40,9 +17,9 @@ func TestGetSuccess(t *testing.T) {
 	expected := MockStruct{Field1: "Field1", Field2: "Field2"}
 	dest := MockStruct{}
 
-	client := newTestClient(func(req *http.Request) *http.Response {
+	client := mock.NewTestClient(func(req *http.Request) *http.Response {
 		assert.Equal(t, req.URL.String(), path)
-		return mockResponse(expected, 200)
+		return mock.Response(expected, 200)
 	})
 
 	_ = Get(client, path, &dest)
@@ -51,8 +28,8 @@ func TestGetSuccess(t *testing.T) {
 }
 
 func TestGetFail_Not200(t *testing.T) {
-	client := newTestClient(func(req *http.Request) *http.Response {
-		return mockResponse([]string{}, 400)
+	client := mock.NewTestClient(func(req *http.Request) *http.Response {
+		return mock.Response([]string{}, 400)
 	})
 	dest := struct{}{}
 	err := Get(client, "", &dest)
@@ -64,9 +41,9 @@ func TestGetFail_UnmarshalWrongStruct(t *testing.T) {
 	path := "some/path"
 	dest := MockStruct{}
 
-	client := newTestClient(func(req *http.Request) *http.Response {
+	client := mock.NewTestClient(func(req *http.Request) *http.Response {
 		assert.Equal(t, req.URL.String(), path)
-		return mockResponse([]byte("xxx"), 200)
+		return mock.Response([]byte("xxx"), 200)
 	})
 
 	err := Get(client, path, &dest)
